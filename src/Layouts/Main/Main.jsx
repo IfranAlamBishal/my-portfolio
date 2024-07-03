bf8f8563c76042f9b443eeb7e34781b9000f34a4
Your NameIfranAlamBishal <you@example.comifranalam4089@gmail.com>
2024-07-04 03:07:20 +0600

    add: footer added

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Pages/Header/Navbar/Navbar";
+import Footer from "../../Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
