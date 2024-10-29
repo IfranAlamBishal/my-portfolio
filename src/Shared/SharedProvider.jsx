1908669c3669430df42315cd2d1f446565781c52
Your NameIfranAlamBishal <ifranalam4089@gmail.com>
2024-10-29 21:56:14 +0600

    add: Added firebase keys and deploy the site on firebase.

--- src/Shared/SharedProvider.jsx
+++ src/Shared/SharedProvider.jsx
@@ -1,17 +1,33 @@
-import { createContext, useRef } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 
-export const SharedContext = createContext(null)
+export const SharedContext = createContext(null);
+const auth = getAuth(app);
 
 const SharedProvider = ({ children }) => {
 
-
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const contacts = useRef(null);
 
     const scrollToContact = () => {
         contacts.current.scrollIntoView({ behavior: 'smooth' });
     }
 
+    useEffect(() => {
+        const unSUbscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unSUbscribe();
+        }
+    }, []);
+
     const contextItems = {
+        user,
+        loading,
         contacts,
         scrollToContact,
     }
